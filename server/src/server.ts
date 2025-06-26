47e59defc6a87fb1d56c8d1161c286e023189d60
rxgq <ashtondunderdale@gmail.com>
2025-06-26 21:19:45 +0100

    remove server

--- server/src/server.ts
+++
@@ -1,6 +0,0 @@
-import app from './app';
-import config from './config/config';
-
-app.listen(config.port, () => {
-    console.log(`Primrose server (${config.nodeEnv}) is listening at http://localhost:${config.port}`);
-});
