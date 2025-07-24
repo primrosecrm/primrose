2bbf00ddd6dce7364b43978c7dd61272a5db55e3
rxgq <ashtondunderdale@gmail.com>
2025-07-24 09:59:17 +0100

    remove App.css import

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,6 @@ import { Route, Routes } from 'react-router-dom'
 import LoginPage from '../features/auth/components/LoginPage'
 import HomePage from '../features/home/HomePage'
 import NotFoundPage from '../features/misc/NotFoundPage'
-import '../App.css'
 
 const App = () => {
   return (
